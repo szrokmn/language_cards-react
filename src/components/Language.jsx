2d7da6c323fd8f1800e5d6a6b3e3960766c33455
Sezer Ökmen <szrokmn34@gmail.com>
2022-12-10 10:24:37 +0300

    add useState

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 
 const Language = ({ name, img, options }) => {
-   
+  const [showDesc, setShowDesc] = useState(false);
+  console.log(showDesc);
   return (
-    <Container style={{background:"peachpuff"}} className="p-4 rounded-3">
-        <Image src={img} width="60%"></Image>
-        <h3 className="display-6">{name}</h3>
+    <Container
+      style={{ background: "peachpuff" }}
+      className="p-4 rounded-3"
+      onClick={() => setShowDesc(!showDesc)}
+    >
+      {!showDesc && (
+        <Container>
+          <Image src={img} width="60%"></Image>
+          <h3 className="display-6">{name}</h3>
+        </Container>
+      )}
+      {showDesc && (
+        <ol>
+          {options.map((item) => {
+            return <li className="h5 text-start">{item}</li>;
+          })}
+        </ol>
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default Language
+export default Language;
